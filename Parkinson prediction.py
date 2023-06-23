d92ca20704e1ce79054abe8a6b400cb5a426200e
Surabhi Singh <112486733+Julimooli@users.noreply.github.com>
2023-06-23 20:04:45 +0530

    Update Parkinson prediction.py

--- Parkinson prediction.py
+++ Parkinson prediction.py
@@ -2,7 +2,11 @@
 # coding: utf-8
 
 # # All Classifier with heat maps
-# # Parkinson Disease prediction
+# # Parkinson's Disease prediction
+# This dataset contains  biomedical voice measurements from 31 people, 23 with Parkinson disease
+# in total 195 rows and 24 columns where the first column is about name, last is status if person is healthy or having disease 
+# in between columns are particular voice measures.
+
 
 # In[1]:
 
